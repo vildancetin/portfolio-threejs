35dd6391f7ac7a7e791c8026a3b190a824162272
Vildan Çetin <vcetin1356@gmail.com>
2025-11-22 20:13:44 +0300

    refactor:update button link

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         },2000)
     }
   return (
-    <section className='c-space my-20'>
+    <section className='c-space my-20' id='about'>
         <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
             <div className='col-span-1 xl:row-span-3'>
                 <div className='grid-container'>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -73,7 +73,7 @@ const Hero = () => {
             </Canvas>
         </div>
         <div className='absolute bottom-7 left-0 right-0 w-full z-10 c-space'>
-          <a href='#contact' className='w-fit'>
+          <a href='#about' className='w-fit'>
             <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
           </a>
         </div>

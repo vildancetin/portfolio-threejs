c67ef3fd2654803687244fee2cd78d5f2e459680
Vildan Çetin <vcetin1356@gmail.com>
2025-12-04 21:53:47 +0300

    updated contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -56,7 +56,7 @@ const Contact = () => {
 
         <div className="contact-container">
           <h3 className="head-text">Let's talk</h3>
-          <p className="text-lg text-white-600 mt-3">
+          <p className="text-lg text-white-600">
             Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
             life, I’m here to help.
           </p>
